2b6da778c219e78e651a8c0e3c35a5671593895a
Karacky <mrnk3078@gmail.com>
2021-02-19 14:51:41 +0900

    fix: enable UserEntity to return null error

--- domain/user/entity.go
+++ domain/user/entity.go
@@ -1,5 +1,11 @@
 package user
 
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
+
 // User Entity express user.
 type User struct {
 	id         ID
@@ -9,6 +15,16 @@ type User struct {
 
 // NewUser Constructor generate user entity.
 func NewUser(id ID, name Name, mailAdress MailAdress) (*User, error) {
+
+	if id == ID(uuid.Nil) {
+		return nil, errors.New("UserID is null")
+	}
+	if name == "" {
+		return nil, errors.New("UserName is null")
+	}
+	if mailAdress == "" {
+		return nil, errors.New("UserMailAdress is null")
+	}
 
 	user := new(User)
 	user.id = id
