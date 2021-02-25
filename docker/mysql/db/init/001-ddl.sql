04d7e4e16a89c0c2795f9e10bd9735ec5b27a4ae
Karacky <mrnk3078@gmail.com>
2021-02-25 19:04:01 +0900

    feat: User作成ddl

---
+++ docker/mysql/db/init/001-ddl.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS `ddd_sample`;
+USE `ddd_sample`;
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id`          VARCHAR(255) NOT NULL COMMENT 'ユーザID',
+  `name`        VARCHAR(255) NOT NULL COMMENT 'ユーザ名',
+  `mail_adress` VARCHAR(255) NOT NULL COMMENT 'メールアドレス',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_mail_adress` (`mail_adress`))
+ENGINE=InnoDB
+COMMENT='ユーザ'
+DEFAULT CHARSET=utf8mb4;
