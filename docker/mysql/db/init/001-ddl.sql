bcf8ededfbb6c6dfcd8df96d2acf33a55f1b2559
Karacky <mrnk3078@gmail.com>
2021-03-02 18:15:18 +0900

    feat: Userテーブルにhash化したパスワードのカラム追加

--- docker/mysql/db/init/001-ddl.sql
+++ docker/mysql/db/init/001-ddl.sql
@@ -4,9 +4,10 @@ USE `ddd_sample`;
 DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id`           CHAR(26)     NOT NULL COMMENT 'ユーザID',
-  `name`         VARCHAR(64)  NOT NULL COMMENT 'ユーザ名',
-  `mail_address` VARCHAR(255) NOT NULL COMMENT 'メールアドレス',
+  `id`              CHAR(26)     NOT NULL COMMENT 'ユーザID',
+  `name`            VARCHAR(64)  NOT NULL COMMENT 'ユーザ名',
+  `mail_address`    VARCHAR(255) NOT NULL COMMENT 'メールアドレス',
+  `hashed_password` VARCHAR(255) NOT NULL COMMENT 'パスワード',
   PRIMARY KEY (`id`),
   UNIQUE KEY `idx_mail_address` (`mail_address`))
 ENGINE=InnoDB
